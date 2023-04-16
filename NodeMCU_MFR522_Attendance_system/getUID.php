7cc494195e19dad18c902787a88f58f7deede9b0
Tayef Mahmud <51941846+tayefmahmud63@users.noreply.github.com>
2023-04-16 14:03:57 -0700

    Add files via upload

---
+++ NodeMCU_MFR522_Attendance_system/getUID.php
@@ -0,0 +1,6 @@
+
+<?php
+	$UIDresult=$_POST["UIDresult"];
+	$Write="<?php $" . "UIDresult='" . $UIDresult . "'; " . "echo $" . "UIDresult;" . " ?>";
+	file_put_contents('UIDContainer.php',$Write);
+?>
